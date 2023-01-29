4f7ba813dd9562dff4798ca39d19dac07e0011d8
Angus Leung <aleun016@uottawa.ca>
2023-01-29 01:17:15 -0500

    Most of the code
    I forgot to commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # GBQBot
-Get Broke or Rich Quick! For legal reasons, this bot is not financial advise and I'm not responsible for your failing marriage after using it
+Get Broke or Rich Quick! For legal reasons, this bot is not financial advise and I'm not responsible for your failing marriage after using it. (Yes I'm talking to you r/WSB)
+
+This isn't even done yet...
---
+++ run.py
@@ -0,0 +1,57 @@
+import openai
+
+import json
+import datetime
+import time
+
+import requests
+
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+
+# Keys
+
+app = Flask(__name__)
+
+# Routing
+@app.route('/', methods=['GET', 'POST'])
+def index():
+
+    
+    response = ["", ""]
+
+    if request.method == "POST":
+
+        openai.api_key = ""
+    
+        today = datetime.datetime.now();
+        today = time.mktime(today.timetuple())
+        lastYear = datetime.datetime.now()-datetime.timedelta(weeks=52);
+        lastYear = time.mktime(lastYear.timetuple())
+    
+        # Get all time historical data: https://api.coingecko.com/ping
+        #coinDataQuery = "https://api.coingecko.com/api/v3/coins/" + request.form['query'] + "/market_chart/range?vs_currency=cad&from=" + str(lastYear) +"&to=" + str(today)
+
+        #coinData = requests.get(coinDataQuery)
+
+        #print(coinData.json())
+
+        botResponse = openai.Completion.create(model="text-davinci-003", prompt=request.form['query'], temperature=0, max_tokens=1000)
+
+        response[1] = request.form['query']
+        response[0] = botResponse
+
+        print("Bot Raw Response: ", botResponse)
+        print("Bot Text Response: ", response[0])
+        print("Last Line: ", response[1])
+
+        response[0] = botResponse["choices"][0]["text"]
+
+        return render_template('index.html', response=response)
+    
+    else:
+        botResponse = ""
+
+        return render_template('index.html', response=response)
